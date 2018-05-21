31be0dd211c95ff645dfcc4bd65a67daed2c4fa2
pandec <bbdecyk@gmail.com>
2018-05-21 12:02:22 +0200

    rest call test update 18

--- modules/quote.js
+++ modules/quote.js
@@ -2,13 +2,14 @@
 
 let auth = require("./slack-salesforce-auth"),
     force = require("./force"),
-    QUOTE_TOKEN = process.env.SLACK_QUOTE_TOKEN;
+    QUOTE_TOKEN = process.env.SLACK_QUOTE_TOKEN,
+    APP_TOKEN = process.env.SLACK_APP_TOKEN;
 
 exports.execute = (req, res) => {
 
     console.log('bdec // token: ' + req.body.token);
 
-    if (req.body.token != QUOTE_TOKEN) {
+    if (req.body.token != QUOTE_TOKEN && req.body. != APP_TOKEN) {
         console.log("Invalid token");
         res.send("Invalid token");
         return;
@@ -50,13 +51,37 @@ exports.execute = (req, res) => {
                     let fields = [];
                     fields.push({title: "Number", value: quote.zqu__Number__c, short: true});
                     fields.push({title: "Status", value: quote.zqu__Status__c, short: true});
+
+
+
                     attachments.push({
                         author_name: quote.CreatedBy.Name,
                         author_link: oauthObj.instance_url + "/" + quote.CreatedBy.Id,
                         title: quote.Name,
                         title_link: oauthObj.instance_url + "/" + quote.Id,
                         color: "#1798c1",
-                        fields: fields
+                        fields: fields,
+                        actions: [
+                            {
+                                "name": "yes",
+                                "text": "yes",
+                                "type": "button",
+                                "value": "yes"
+                            },
+                            {
+                                "name": "no",
+                                "text": "no",
+                                "type": "button",
+                                "value": "no"
+                            },
+                            {
+                                "name": "maybe",
+                                "text": "maybe",
+                                "type": "button",
+                                "value": "maybe",
+                                "style": "danger"
+                            }
+                        ]
                     });
                 });
                 res.json({text: "Quotes matching '" + req.body.text + "':", attachments: attachments});
